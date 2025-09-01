434f395bc704cb4c499f6b68bd527bd37fba205c
Matvei <mets0n18@vk.com>
2025-09-01 16:33:12 +0300

    Initial Commit

---
+++ CosmicCanvas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CosmicCanvas
+//
+//  Created by Матвей Авдеев on 01.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
